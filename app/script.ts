f3e5892d26e4e9e40af12c7383a2becfeca8cac0
thisisjofrank <jofranchetti@gmail.com>
2022-03-03 23:21:29 +0000

    styles

--- app/script.ts
+++ app/script.ts
@@ -2,12 +2,6 @@ import { Game } from "./games/rps/Game";
 
 console.log("Oh hai! 🖤");
 
-const result = await fetch("/api/ably-token-request");
-const data = await result.json();
-console.log(data);
-
-document.getElementById("response").innerText = JSON.stringify(data);
-
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 const rpsInstance = new Game("my-game-id", canvas);
 
