3e08e347a2f43322e44f6efc7600b19b3276df01
thisisjofrank <jofranchetti@gmail.com>
2022-03-20 16:39:09 +0000

    Expanded tests to cater for merging scoreboards.

--- app/arcade-ui-helpers/Scoreboard.test.ts
+++ app/arcade-ui-helpers/Scoreboard.test.ts
@@ -8,6 +8,31 @@ describe("Scoreboard", () => {
         sut = new Scoreboard();
     });
 
+    it("can initilise with a set of scores", () => {
+        sut = new Scoreboard([
+            { name: "a", score: 1 }, 
+            { name: "b", score: 1 }
+        ]);
+
+        expect(sut.scores.length).toBe(2);
+        expect(sut.scores[0].name).toBe("a");
+        expect(sut.scores[1].name).toBe("b");
+    })
+
+    it("can initilise with a set of scores and a cap", () => {
+        sut = new Scoreboard([
+            { name: "a", score: 1 }, 
+            { name: "b", score: 1 },
+            { name: "c", score: 1 },
+            { name: "d", score: 1 },
+        ], 3);
+
+        expect(sut.scores.length).toBe(3);
+        expect(sut.scores[0].name).toBe("a");
+        expect(sut.scores[1].name).toBe("b");
+        expect(sut.scores[2].name).toBe("c");
+    })
+
     it("addRange, adds supplied scores", () => {
         sut.addRange([
             { name: "a", score: 1 }, 
@@ -28,5 +53,23 @@ describe("Scoreboard", () => {
         expect(sut.scores.length).toBe(2);
         expect(sut.scores[0].name).toBe("b")
         expect(sut.scores[1].name).toBe("a");
+    });
+
+    it("addRange, applies cap after new items added", () => {
+        sut = new Scoreboard([
+            { name: "a", score: 10 }, 
+            { name: "b", score: 9 },
+            { name: "c", score: 5 },
+        ], 3);
+
+        sut.addRange([
+            { name: "d", score: 6 },
+            { name: "e", score: 2 },
+        ]);
+
+        expect(sut.scores.length).toBe(3);
+        expect(sut.scores[0].name).toBe("a")
+        expect(sut.scores[1].name).toBe("b");
+        expect(sut.scores[2].name).toBe("d");
     });
 });
